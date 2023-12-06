95c01318e9ad6fbfcea523f93ac516f13f7dbd76
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-12-06 08:16:06 -0300

    sistema funções

--- src/application/Sistema.java
+++ src/application/Sistema.java
@@ -9,45 +9,68 @@ import entities.Paciente;
 import entities.Pagamento;
 
 public class Sistema {
-	
+
 	private ArrayList<FichaAtendimento> fichasChamadas = new ArrayList<>();
 	private ArrayList<Paciente> pacientes = new ArrayList<>();
 	private ArrayList<Dentista> dentistas = new ArrayList<>();
 	private ArrayList<Atendente> atendentes = new ArrayList<>();
 	private ArrayList<Pagamento> pagamentos = new ArrayList<>();
-	
+
 	public Sistema() {
-		
+
 	}
-		
+
 	public FichaAtendimento chamarProximo() {
 		return fichasChamadas.get(0);
 	};
-	
+
 	public void concluirAtendimento() {
-		Pagamento p = new Pagamento( fichasChamadas.get(0) );
+		Pagamento p = new Pagamento(fichasChamadas.get(0));
 		pagamentos.add(p);
 		fichasChamadas.get(0).setAtendido(true);
 		fichasChamadas.remove(0);
 	}
-	
+
 	public String pagar(String tipo) {
 		String texto = pagamentos.get(0).realizarPagamento(tipo);
 		pagamentos.remove(0);
 		return texto;
 	}
 
-	public boolean verificarCadastro(Long cpf) {
-				
-		for (Paciente paciente : pacientes) {
-			if (paciente.getCpf() ==  cpf) {
+	public boolean verificarCadastroDentistas(Long id) {
+
+		for (Dentista dentista : dentistas) {
+			if (dentista.getId().compareTo(id) == 0) {
 				return true;
 			}
 		}
-		
+
 		return false;
-	}		
+	}
 	
+	public boolean verificarCadastroAtendentes(Long id) {
+
+		for (Atendente atendente : atendentes) {
+			if (atendente.getId().compareTo(id) == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	public boolean verificarCadastro(Long cpf) {
+
+		for (Paciente paciente : pacientes) {
+			if (paciente.getCpf() == cpf) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
 	public ArrayList<FichaAtendimento> getFichasChamadas() {
 		return fichasChamadas;
 	}
@@ -87,8 +110,25 @@ public class Sistema {
 	public void addPagamentos(Pagamento pagamento) {
 		this.pagamentos.add(pagamento);
 	}
+
+	public void removeAtendente(Long id) {
+
+        for (Atendente atendente : atendentes) {
+            if (atendente.getId().compareTo(id) == 0) {
+                atendentes.remove(atendente);
+            }
+        }
+
+    }
+	public void removeDentista(Long id) {
+
+		for (Dentista dentista : dentistas) {
+			if (dentista.getId().compareTo(id) == 0) {
+				atendentes.remove(dentista);
+			}
+		}
+	}
 	
 	
-		
 
 }
